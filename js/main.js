b6e05e3c2daba4e80c0211b6717b1b896f48e086
Dudex11c <55777288+Dudex11c@users.noreply.github.com>
2020-06-24 14:14:53 -0400

    Retreiving Data from api instead

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 
 function ParseSpreadsheet(data) {
     PONYPARAMS = {}
-    console.log(data.feed)
+    console.log(data)
     $(data.feed.entry).each((index, entry) => {
         console.log(entry.content.$t)
     })
 }
 
+function html_loaded() {
+    // Get Pony Parameters Spreadsheet data
+    $.ajax({
+
+        url : 'https://sheets.googleapis.com/v4/spreadsheets/17fPtZaia9huJ5zzr4qS-vFKH8ZO9EKCF7GH5-5GmyYA/?key=AIzaSyBXseFNL191-4HO4bZV-JcgEUxnm7aW9xQ&includeGridData=true',
+        type : 'GET',
+        dataType:'json',
+        success : function(data) {              
+            PONYPARAMS = data;
+        },
+        error : function(request,error)
+        {
+            console.log(JSON.stringify(request));
+            alert("Failed to load Pony Parameters Spreadsheet, resorting to local backup.");
+        }
+    });
+}
+
+function process_pony_params() {
+    
+}
+
 function Roll() {
-    $("#result")[0].innerText = JSON.stringify(roll_pony())
+    // $("#result").innerText = JSON.stringify(roll_pony())
 }
 
 function roll_pony() {
