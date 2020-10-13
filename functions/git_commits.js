31e1c4e546a89b51263077024948a8af66244570
Dudex <ajcicilio@gmail.com>
2020-10-13 14:00:42 -0400

    Username changed

--- functions/git_commits.js
+++ functions/git_commits.js
@@ -8,7 +8,7 @@ exports.handler = async function(event, context, callback) {
         // "Authorization" : "Basic " + btoa("LittlePonyTales:" + token)
         "Authorization" : "Token " + token
     }
-    const url = "https://api.github.com/repos/Dudex11c/PonyTool/commits";
+    const url = "https://api.github.com/repos/Dudex11a/PonyTool/commits";
     const response = await fetch(url, {
         "method" : "GET",
         "headers": headers
